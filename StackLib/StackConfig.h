2b44dc6dda8e01609292a690d8be84c6f5386ec6
Artem Puzankov <71190274+hellopuza@users.noreply.github.com>
2021-04-23 19:57:37 +0300

    new Types.h file

--- StackLib/StackConfig.h
+++ StackLib/StackConfig.h
@@ -13,9 +13,8 @@
 #define STACK_CONFIG_H_INCLUDED
 
 
-#include <limits.h>
+#include "../Types.h"
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 
@@ -39,37 +38,6 @@
 
 
 static const char* stack_logname = "stack.log";
-
-
-template<typename TYPE> const TYPE POISON;
-
-template<> const double POISON<double> = NAN;
-template<> const float  POISON<float>  = NAN;
-template<> const int    POISON<int>    = INT_MAX;
-template<> const size_t POISON<size_t> = UINT_MAX;
-template<> const char   POISON<char>   = '\0';
-template<>       char*  POISON<char*>  = nullptr;
-
-
-template<typename TYPE> const char* PRINT_TYPE;
-
-template<> const char* PRINT_TYPE<double> = "double";
-template<> const char* PRINT_TYPE<float>  = "float";
-template<> const char* PRINT_TYPE<int>    = "int";
-template<> const char* PRINT_TYPE<size_t> = "size_t";
-template<> const char* PRINT_TYPE<char>   = "char";
-template<> const char* PRINT_TYPE<char*>  = "char*";
-
-
-template<typename TYPE> const char* PRINT_FORMAT;
-
-template<> const char* PRINT_FORMAT<double> = "%lf";
-template<> const char* PRINT_FORMAT<float>  = "%f";
-template<> const char* PRINT_FORMAT<int>    = "%d";
-template<> const char* PRINT_FORMAT<size_t> = "0x%08X";
-template<> const char* PRINT_FORMAT<char>   = "%c";
-template<> const char* PRINT_FORMAT<char*>  = "%s";
-
 
 static const size_t MAX_STACK_NUM = 100;
 static const size_t MAX_CAPACITY  = 100000;
